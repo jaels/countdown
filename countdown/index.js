4a6e45b082abf7c42033949c53c1a539ace4c2fa
Yael Stein <yaelst@gmail.com>
2016-09-26 16:24:03 +0200

    countdown

---
+++ countdown/index.js
@@ -0,0 +1,11 @@
+
+
+var countdown = require('./countdown');
+
+new countdown.Countdown(10).on('secondElapsed', function(n) {
+    if (n) {
+        console.log(n + '!');
+    } else {
+        console.log('Lift off!');
+    }
+});
